834815be0027412a74495a2ab51cb139887d0b27
CancisioKarl <cancisiokarlangelo76@gmail.com>
2024-06-15 15:08:41 +0800

    medical record model

--- hospital-backend/app/Models/MedicalRecord.php
+++ hospital-backend/app/Models/MedicalRecord.php
@@ -5,26 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MedicalRecord extends Model
+class Medicalrecord extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'patient_id',
-        'doctor_id',
-        'visit_date',
-        'diagnosis',
-        'treatment',
-        'notes'
-    ];
-
-    public function patient()
-    {
-        return $this->belongsTo(Patient::class);
-    }
-
-    public function doctor()
-    {
-        return $this->belongsTo(Doctor::class);
-    }
+    public $table = 'tblMedicalRecord';
 }
